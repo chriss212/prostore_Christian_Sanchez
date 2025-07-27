116ee72dab9e7fe1de01b77a092a6adffbc8ec8e
chriss212 <20235787@esen.edu.sv>
2025-07-27 13:47:19 -0600

    deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
